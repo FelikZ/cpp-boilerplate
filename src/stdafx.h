2bae25d3f81fa391efcc9193be07967f8dc8b70a
FelikZ <thefelikz@gmail.com>
2018-03-12 19:46:29 +0100

    Fix debug mode for windows and supress some warnings

--- src/stdafx.h
+++ src/stdafx.h
@@ -5,10 +5,13 @@
 
 #pragma once
 
+#pragma warning(push)
+#pragma warning(disable : 4180)
 #include <iostream>
 #include <tbb/concurrent_queue.h>
 #include <tbb/parallel_invoke.h>
 #include <tbb/task_group.h>
 #include <thread>
+#pragma warning(pop)
 
 // TODO: reference additional headers your program requires here
